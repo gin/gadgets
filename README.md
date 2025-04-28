b896ac9501640f5f4191b84c702ed5324697a201
Luigi <luigi@umbrellavalidator.com>
2025-04-27 23:23:36 -0700

    Add Bank

--- README.md
+++ README.md
@@ -5,9 +5,16 @@ The smart contracts have their corresponding tests in the `test/` directory.
 |----------------------------------------------|-------------------------------------------------------------------------|
 | [CommunityChest.sol](src/CommunityChest.sol) | An update to the smart contract from [#communitychest](#communitychest) |
 | [TipJar.sol](src/TipJar.sol)                 | An update to the smart contract from [#tipjar](#tipjar)                 |
+| [Bank.sol](src/Bank.sol)                     | An update to the smart contract from [#bank](#bank)                     |
 
 [communitychest]: https://programtheblockchain.com/posts/2017/12/15/writing-a-contract-that-handles-ether/
 [tipjar]: https://programtheblockchain.com/posts/2017/12/26/checking-the-sender-in-a-smart-contract/
+[bank]: https://programtheblockchain.com/posts/2018/01/05/writing-a-banking-contract/
+[savings]: https://programtheblockchain.com/posts/2018/01/12/writing-a-contract-that-handles-time/
+[crowdfunding]: https://programtheblockchain.com/posts/2018/01/19/writing-a-crowdfunding-contract-a-la-kickstarter/
+[multicounter]: https://programtheblockchain.com/posts/2018/01/24/logging-and-watching-solidity-events/
+[minimaltoken]: https://programtheblockchain.com/posts/2018/01/26/what-is-an-ethereum-token/
+[simpleerc20token]: https://programtheblockchain.com/posts/2018/01/30/writing-an-erc20-token-contract/
 
 ### Notes
 Show which functions are not tested
---
+++ src/Bank.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.29;
+
+contract Bank {
+    error Unauthorized();
+    error WithdrawUnsuccessful(uint256);
+    error InvalidAmount();
+    error InsufficientBalance(uint256);
+
+    mapping(address => uint256) public balances;
+    address public owner;
+
+    modifier onlyOwner() {
+        if (msg.sender != owner) revert Unauthorized();
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function changeOwner(address _newOwner) external onlyOwner {
+        owner = _newOwner;
+    }
+
+    function deposit(uint256 _amount) payable external {
+        if (msg.value != _amount) revert InvalidAmount();
+        balances[msg.sender] += _amount;
+    }
+
+    function withdraw(uint256 _amount) external {
+        if (balances[msg.sender] < _amount) revert InsufficientBalance(balances[msg.sender]);
+        balances[msg.sender] -= _amount;
+        (bool success, ) = msg.sender.call{value: _amount}("");
+        if (!success) revert WithdrawUnsuccessful(_amount);
+    }
+
+    function transfer(address _to, uint256 _amount) external {
+        if (balances[msg.sender] < _amount) revert InsufficientBalance(balances[msg.sender]);
+        balances[msg.sender] -= _amount;
+        balances[_to] += _amount;
+    }
+
+    function getBalance(address _user) external view returns (uint256) {
+        return balances[_user];
+    }
+
+    receive() external payable {}
+    fallback() external payable {}
+}
